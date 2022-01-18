7cf87414a057d13d0f8863c6aaddf57b55db3e2e
Kamruzzaman <zaman7u@gmail.com>
2022-01-18 12:57:07 +0600

    update app init route

--- routes/api.php
+++ routes/api.php
@@ -31,40 +31,39 @@ Route::post('form-submitted', function (Request $request) {
     return $request->all();
 });
 
-Route::prefix('v1')->group(function () {
 
-    Route::prefix('auth')->group(function () {
-        //get current user
-        Route::get('current-user', [AuthenticatedSessionController::class, 'currentUser']);
+Route::prefix('auth')->group(function () {
+    //get current user
+    Route::get('current-user', [AuthenticatedSessionController::class, 'currentUser']);
 
-        //login routes
-        Route::post('login', [AuthenticatedSessionController::class, 'login']);
-        Route::post('register', [AuthenticatedSessionController::class, 'register']);
+    //login routes
+    Route::post('login', [AuthenticatedSessionController::class, 'login']);
+    Route::post('register', [AuthenticatedSessionController::class, 'register']);
 
-        //logout
-        Route::post('logout', [AuthenticatedSessionController::class, 'logout']);
+    //logout
+    Route::post('logout', [AuthenticatedSessionController::class, 'logout']);
 
-    });
-
-    Route::get('init', [HomeController::class, 'index']);
-    Route::get('user', [HomeController::class, 'user']);
-    //get category
-    Route::get('category', [CategoryController::class, 'index']);
-    //get post
-    Route::get('posts', [PostController::class, 'index']);
-    Route::get('post', [PostController::class, 'show']);
-
-    // comment route
-    Route::post('comment', [CommentController::class, 'store']);
-    Route::post('comment', [CommentController::class, 'destroy']);
-
-    // chat
-    Route::post('get-message', [\App\Http\Controllers\API\ChatController::class, 'getMessage']);
-
-    // upload-image-by-cropper js
-    Route::post('upload-image-by-croperjs', [HomeController::class, 'uploadImageByCroperjs']);
-    Route::post('upload-docs-file', [HomeController::class, 'uploadDocsFile']);
 });
+
+Route::get('init-app', [HomeController::class, 'index']);
+Route::get('user', [HomeController::class, 'user']);
+//get category
+Route::get('category', [CategoryController::class, 'index']);
+//get post
+Route::get('posts', [PostController::class, 'index']);
+Route::get('post', [PostController::class, 'show']);
+
+// comment route
+Route::post('comment', [CommentController::class, 'store']);
+Route::post('comment', [CommentController::class, 'destroy']);
+
+// chat
+Route::post('get-message', [\App\Http\Controllers\API\ChatController::class, 'getMessage']);
+
+// upload-image-by-cropper js
+Route::post('upload-image-by-croperjs', [HomeController::class, 'uploadImageByCroperjs']);
+Route::post('upload-docs-file', [HomeController::class, 'uploadDocsFile']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
