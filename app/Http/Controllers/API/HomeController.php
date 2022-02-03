918b4617c323fdbb303c891be984fe46c51a8792
Kamruzzaman <zaman7u@gmail.com>
2022-02-04 00:47:28 +0600

    bugs fixing

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
         $user = User::find($id);
         return response()->json([
             'success' => true,
-            'users' => $user
+            'user' => $user
         ], 200);
 
     }
