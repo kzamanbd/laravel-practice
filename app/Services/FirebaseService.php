009a49927a4cf8a350adbb68429e307cc0cf69d0
Kamruzzaman <zamandotbd@gmail.com>
2022-07-12 20:15:11 +0600

    update firebase

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -7,7 +7,7 @@ use Kreait\Firebase\Factory;
 class FirebaseService {
     
     private static function createFactory(){
-        return (new Factory)->withServiceAccount(base_path("firebase.json"))->withDatabaseUri("https://notification-forwarder-22-default-rtdb.asia-southeast1.firebasedatabase.app");
+        return (new Factory)->withServiceAccount(base_path("firebase.json"))->withDatabaseUri("https://ai-notifier-fc-default-rtdb.europe-west1.firebasedatabase.app");
     }
 
     public static function createDatabase(){
