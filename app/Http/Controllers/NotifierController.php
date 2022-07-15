302f668f2915be0482a9885faf07e89720e2e4bc
Kamruzzaman <zamandotbd@gmail.com>
2022-07-15 16:22:59 +0600

    Update NotifierController.php

--- app/Http/Controllers/NotifierController.php
+++ app/Http/Controllers/NotifierController.php
@@ -16,13 +16,13 @@ class NotifierController extends Controller
      */
     function store(Request $request)
     {
-        $androidText = $request->input('android_text');
-        if (Str::contains($request->input('android_title'), "bKash")) {
-            $secondArray = explode("TrxID ", $androidText);
-            $transactionId = isset($secondArray[1]) ? explode(" ", $secondArray[1])[0] : null;
-        } else if (Str::contains($request->input('android_title'), "NAGAD")) {
-            $secondArray = explode("TxnID:", $androidText);
-            $transactionId = isset($secondArray[1]) ? explode("Comm:", $secondArray[1])[0] : null;
+        $androidText = Str::replace('\n', ' ', $request->input('android_text'));
+        if (Str::contains($request->input('android_title'), 'bKash')) {
+            $secondArray = explode('TrxID ', $androidText);
+            $transactionId = isset($secondArray[1]) ? explode(' ', $secondArray[1])[0] : null;
+        } else if (Str::contains($request->input('android_title'), 'NAGAD')) {
+            $secondArray = explode('TxnID: ', $androidText);
+            $transactionId = isset($secondArray[1]) ? explode(' ', $secondArray[1])[0] : null;
         } else {
             $transactionId = null;
         }
@@ -33,10 +33,10 @@ class NotifierController extends Controller
             $arrayMarge = array_merge($request->all(), $newArray);
             $message = MessageNotifier::create($arrayMarge);
         }
-        if ($request->input('package_name') == "com.konasl.nagad" or $request->input('package_name') == "com.konasl.nagad.agent") {
+        if ($request->input('package_name') == 'com.konasl.nagad' or $request->input('package_name') == 'com.konasl.nagad.agent') {
             // get lest 11 digits
-            $numberArray = explode(":", $androidText);
-            $mobileNumber = str_replace("-", "", trim(end($numberArray)));
+            $numberArray = explode(':', $androidText);
+            $mobileNumber = Str::replace('-', '', trim(end($numberArray)));
             if (strlen($mobileNumber) == 11) {
                 $inputArray = [
                     'mobile' => $mobileNumber,
@@ -50,13 +50,13 @@ class NotifierController extends Controller
 
         if (isset($message) or isset($newNagad)) {
             return response()->json([
-                "status" => true,
-                "message" => "Successfully Added",
+                'status' => true,
+                'message' => 'Successfully Added',
             ]);
         }
         return response()->json([
-            "status" => false,
-            "message" => "Not found!",
+            'status' => false,
+            'message' => 'Not found!',
         ]);
     }
 
@@ -77,10 +77,10 @@ class NotifierController extends Controller
         $countNotification = count($notifications);
         $countNagad = count($nagadMessages);
         return response()->json([
-            "status" => true,
-            "message" => "Total $countNotification Notification and $countNagad Mobile Number Founds!",
-            "nagad_messages" => $nagadMessages,
-            "notifications" => $notifications
+            'status' => true,
+            'message' => "Total $countNotification Notification and $countNagad Mobile Number Founds!",
+            'nagad_messages' => $nagadMessages,
+            'notifications' => $notifications
         ]);
     }
 }
