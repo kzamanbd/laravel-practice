48c972f64637744af3f17aee5275db5f0d2c4ca5
Kamruzzaman <zamandotbd@gmail.com>
2022-07-24 21:02:24 +0600

    update notification controller

--- app/Http/Controllers/NotifierController.php
+++ app/Http/Controllers/NotifierController.php
@@ -4,65 +4,111 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use App\Models\NagadNumber;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Str;
 
 class NotifierController extends Controller
 {
+    const BKASH = "BKASH";
+    const NAGAD = "NAGAD";
+    const ROCKET = "ROCKET";
+    const UPAY = "UPAY";
+
     /**
      * @param Request $request
      * @return JsonResponse
      */
     function store(Request $request)
     {
-        $androidText = Str::replace('\n', ' ', $request->input('android_text'));
-        if (Str::contains($request->input('android_title'), 'bKash')) {
-            $secondArray = explode('TrxID ', $androidText);
-            $transactionId = isset($secondArray[1]) ? explode(' ', $secondArray[1])[0] : null;
-        } else if (Str::contains($request->input('android_title'), 'NAGAD')) {
-            $secondArray = explode('TxnID: ', $androidText);
-            $transactionId = isset($secondArray[1]) ? explode(' ', $secondArray[1])[0] : null;
-        } else {
-            $transactionId = null;
+        $transactionId = self::getTransactionId($request->input('android_title'), $request->input('android_text'));
+        if (Message::query()->where('transaction_id', trim($transactionId))->exists()) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Already Exists',
+            ]);
         }
-        if ($transactionId != null) {
-            $newArray = [
-                'transaction_id' => trim($transactionId),
-            ];
-            $arrayMarge = array_merge($request->all(), $newArray);
-            $message = Message::create($arrayMarge);
-        } else {
-            $message = null;
-        }
-        $newNagad = null;
-        if ($request->input('package_name') == 'com.konasl.nagad' or $request->input('package_name') == 'com.konasl.nagad.agent') {
-            // get lest 11 digits
-            $numberArray = explode(':', $androidText);
-            $mobileNumber = Str::replace('-', '', trim(end($numberArray)));
-            if (strlen($mobileNumber) == 11) {
-                $inputArray = [
-                    'mobile' => $mobileNumber,
-                    'android_text' => trim($androidText),
-                    'android_title' => trim($request->input('android_title')),
-                    'package_name' => $request->input('package_name'),
-                ];
-                $newNagad = NagadNumber::create($inputArray);
-            }
-        }
+        $message = Message::create([
+            'android_title' => $request->input('android_title'),
+            'android_text' => $request->input('android_text'),
+            'transaction_id' => trim($transactionId),
+            'msg_from' => substr($request->input('msg_from'), -11),
+        ]);
 
-        if (isset($message) or isset($newNagad)) {
+        if (isset($message)) {
             return response()->json([
                 'status' => true,
                 'message' => 'Successfully Added',
-                'notification' => $message,
-                'nagad' => $newNagad
+                'detail' => $message,
             ]);
         }
         return response()->json([
             'status' => false,
-            'message' => 'Not found!',
+            'message' => 'Failed to add',
         ]);
+    }
+
+    function syncOfflineMessage(Request $request)
+    {
+        if ($request->input('messages')) {
+            $offlineIds = [];
+            foreach (json_decode($request->input('messages'), true) as $message) {
+                $transactionId = self::getTransactionId($message['android_title'], $message['android_text']);
+                if (!Message::query()->where('transaction_id', trim($transactionId))->exists()) {
+                    $newMsg = Message::create([
+                        'android_title' => $message['android_title'],
+                        'android_text' => $message['android_text'],
+                        'transaction_id' => trim($transactionId),
+                        'msg_from' => substr($request->input('msg_from'), -11),
+                        'is_offline' => $message['offline_id'],
+                        'created_at' => Carbon::parse($message['created_at'])->format('Y-m-d H:i:s'),
+                    ]);
+
+                    $offlineIds[] = $newMsg->is_offline;
+                }
+
+            }
+
+            $countIds = count($offlineIds);
+            if ($countIds > 0) {
+                return response()->json([
+                    'status' => true,
+                    'message' => "$countIds Offline Message Synced",
+                    'offlineIds' => $offlineIds
+                ]);
+            }
+            return response()->json([
+                'status' => false,
+                'message' => 'Operation Failed',
+            ]);
+        }
+        return response()->json([
+            'status' => false,
+            'message' => 'Failed to Add',
+        ]);
+    }
+
+    /**
+     * @param string $androidTitle
+     * @param string $androidText
+     * @return string|null
+     */
+    function getTransactionId(string $androidTitle, string $androidText)
+    {
+        if (Str::contains(Str::upper($androidTitle), self::BKASH) or Str::contains(Str::upper($androidTitle), self::UPAY)) {
+            $secondArray = explode('TrxID ', $androidText);
+            $transactionId = isset($secondArray[1]) ? substr($secondArray[1], 0, 10) : null;
+        } else if (Str::contains(Str::upper($androidTitle), self::NAGAD)) {
+            $secondArray = explode('TxnID: ', $androidText);
+            $transactionId = isset($secondArray[1]) ? substr($secondArray[1], 0, 8) : null;
+        } else if (Str::contains(Str::upper($androidTitle), self::ROCKET)) {
+            $secondArray = explode('TxnId: ', $androidText);
+            $transactionId = isset($secondArray[1]) ? substr($secondArray[1], 0, 10) : null;
+        } else {
+            $transactionId = null;
+        }
+        return $transactionId;
     }
 
     /**
@@ -71,20 +117,15 @@ class NotifierController extends Controller
     function getNotification()
     {
         $notifications = Message::query()
-            ->whereBetween('created_at', [now()->subMinutes(4320), now()])
+            ->whereBetween('created_at', [now()->subMinutes(2880), now()])
             ->latest()
-            ->get();
-
-        $nagadMessages = NagadNumber::query()
-            ->whereBetween('created_at', [now()->subMinutes(1440), now()])
-            ->get();
+            ->get(['id', 'android_title', 'android_text', 'transaction_id', 'created_at']);
 
         $countNotification = count($notifications);
-        $countNagad = count($nagadMessages);
         return response()->json([
             'status' => true,
-            'message' => "Total $countNotification Notification and $countNagad Mobile Number Founds!",
-            'nagad_messages' => $nagadMessages,
+            'message' => "Last 2 days total $countNotification messages Found!",
+            'nagad_messages' => [],
             'notifications' => $notifications
         ]);
     }
--- database/migrations/2022_07_14_234301_create_messages_table.php
+++ database/migrations/2022_07_14_234301_create_messages_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,15 +14,16 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->string('app_name')->nullable();
-            $table->string('package_name')->nullable();
             $table->string('android_title')->nullable();
-            $table->string('android_text')->nullable();
-            $table->string('android_sub_text')->nullable();
-            $table->string('android_big_text')->nullable();
-            $table->string('android_info_text')->nullable();
             $table->string('transaction_id')->unique()->nullable();
+            $table->string('android_text')->nullable();
+            $table->string('msg_from')->nullable();
+            $table->string('sender')->nullable();
+            $table->string('receiver')->nullable();
             $table->tinyInteger('is_offline')->nullable();
+            $table->string('status')->nullable()
+                ->default('PENDING')
+                ->comment('PENDING, APPROVED, REJECT');
             $table->timestamps();
         });
     }
