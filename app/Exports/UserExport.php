56728b9d62b96b7bc99a015e716090c5236e55c0
Kamruzzaman <kzamanbn@gmail.com>
2023-02-23 10:44:12 +0600

    added user excel goruping

--- app/Exports/UserExport.php
+++ app/Exports/UserExport.php
@@ -6,8 +6,10 @@ use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class UserExport implements FromView, WithColumnWidths
+class UserExport implements FromView, WithColumnWidths, WithEvents
 {
     /**
      * @return View
@@ -29,6 +31,23 @@ class UserExport implements FromView, WithColumnWidths
             'B' => 30,
             'C' => 30,
             'D' => 20,
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        // after sheet rows grouping
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $event->sheet->getDelegate()->setAutoFilter('A2:D2');
+                // 10 rows grouping
+                // $event->sheet->getDelegate()->groupRows(2, 11, 0);
+
+                // get row dimension
+                foreach (range(2, 11) as $row) {
+                    $event->sheet->getDelegate()->getRowDimension($row)->setOutlineLevel(1);
+                }
+            },
         ];
     }
 }
