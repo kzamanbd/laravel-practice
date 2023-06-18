4deef88e2236a34d1a964a418ff61b8c7e10e937
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-06-19 01:46:39 +0600

    update and added return type

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,4 @@
 <x-app-layout>
-    <x-slot name="title">Dashboard</x-slot>
-
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -10,19 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-            <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                <div class="mt-6 text-gray-500">
-                    Laravel provides a beautiful, robust starting point for your next Laravel application.
-                    Laravel is designed
-                    to help you build your application using a development environment that is simple, powerful, and
-                    enjoyable. We believe
-                    you should love expressing your creativity through programming, so we have spent time carefully
-                    crafting the Laravel
-                    ecosystem to be a breath of fresh air. We hope you love it.
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
