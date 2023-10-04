44844707e991c5f56b42f7f343da28362702710d
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-10-04 14:26:51 +0600

    Update report-layout.blade.php

--- resources/views/components/report-layout.blade.php
+++ resources/views/components/report-layout.blade.php
@@ -1,25 +1,27 @@
 <!doctype html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>{{ $title ?? 'Report' }}</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            color: black;
-            width: 100%;
-            font-family: Calibri;
-            page-break-inside: auto;
-        }
-    </style>
-    {{ $styles ?? '' }}
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>{{ $title ?? 'Report' }}</title>
+        <style>
+            table {
+                width: 100%;
+                color: black;
+                border-collapse: collapse;
+                font-family: Calibri;
+                page-break-inside: auto;
+            }
+        </style>
+        @if (isset($styles))
+            {{ $styles }}
+        @endif
+    </head>
 
-<body>
-    {{ $slot }}
-</body>
+    <body>
+        {{ $slot }}
+    </body>
 
 </html>
