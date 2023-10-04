273780ad8eb398c2a073397687b4a1225d0cfff2
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-10-04 14:31:45 +0600

    Update users.blade.php

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -130,26 +130,22 @@
                                                 </td>
 
                                                 <td class="px-3 py-2 text-sm text-center">
-                                                    <x-dropdown align="right">
-                                                        <x-slot name="trigger">
-                                                            <button type="button">
-                                                                <span class="material-icons">
-                                                                    more_vert
-                                                                </span>
-                                                            </button>
-                                                        </x-slot>
 
-                                                        <x-slot name="content">
-                                                            <x-dropdown-link href="#"
-                                                                wire:click.prevent="editItem({{ $user->id }})">
-                                                                Edit</x-dropdown-link>
-                                                            <x-dropdown-link href="{{ route('user.show', $user->id) }}">
-                                                                View</x-dropdown-link>
-                                                            <x-dropdown-link href="#"
-                                                                wire:click.prevent="deleteItem({{ $user->id }})">
-                                                                Delete</x-dropdown-link>
-                                                        </x-slot>
-                                                    </x-dropdown>
+                                                    <div class="inline-flex rounded-md shadow-sm" role="group">
+                                                        <button type="button"
+                                                            wire:click.prevent="editItem({{ $user->id }})"
+                                                            class="px-2 py-1 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                                                            Edit
+                                                        </button>
+                                                        <a wire:navigate href="{{ route('user.show', $user->id) }}"
+                                                            class="px-2 py-1 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                                                            View
+                                                        </a>
+                                                        <button type="button"
+                                                            class="px-2 py-1 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                                                            Delete
+                                                        </button>
+                                                    </div>
                                                 </td>
                                             </tr>
                                         @empty
