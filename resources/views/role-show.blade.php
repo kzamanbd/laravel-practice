9d4ca28942eb717a10bf4f1c52c268533f75cb3c
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-12-31 22:46:50 +0600

    Update role-show.blade.php

--- resources/views/role-show.blade.php
+++ resources/views/role-show.blade.php
@@ -7,10 +7,8 @@
 
     <div class="py-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-5">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-                </div>
             </div>
         </div>
     </div>
