9d530d541b464722afb13c62a9b888e663e6452e
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-03-06 14:38:44 +0600

    Update account-verification.blade.php

--- resources/views/emails/account-verification.blade.php
+++ resources/views/emails/account-verification.blade.php
@@ -1,7 +1,7 @@
 <div>
     <h1>Account Verification</h1>
     <p>Click the link below to verify your account</p>
-    <a href="{{ $url }}">Verify Account</a>
+    <a href="{{ config('app.url') }}">Verify Account</a>
     <p>Thank you</p>
     <p>Team</p>
     <p>{{ config('app.name') }}</p>
