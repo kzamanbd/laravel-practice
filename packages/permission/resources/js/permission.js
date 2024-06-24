6d04f39202d585d4a2ecc8214464e8dca72ce5c8
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-06-24 17:13:48 +0600

    feat: working with toast js

--- packages/permission/resources/js/permission.js
+++ packages/permission/resources/js/permission.js
@@ -1,1 +1,57 @@
 import "./bootstrap";
+
+document.addEventListener("livewire:init", () => {
+    Livewire.on("confirm-modal", (event) => {
+        const modalData = Array.isArray(event) ? event[0] : event;
+        console.log(modalData);
+        if (confirm(modalData.message || "Are you sure?")) {
+            Livewire.dispatch(modalData.action);
+        }
+    });
+});
+
+import Toastify from "toastify-js";
+
+window.Toastify = Toastify;
+
+const toast = (message, type = "success") => {
+    const types = {
+        success: "#10B981",
+        error: "#EF4444",
+        warning: "#F59E0B",
+        info: "#3B82F6",
+    };
+
+    Toastify({
+        text: message || "An error occurred",
+        duration: 3000,
+        newWindow: true,
+        className: `toast-${type}`,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: types[type],
+        },
+    }).showToast();
+};
+
+const getMessage = (event) => {
+    return event.detail.message || event.detail[0] || "An error occurred";
+};
+document.addEventListener("success", function (event) {
+    toast(getMessage(event), "success");
+});
+
+document.addEventListener("warning", function (event) {
+    toast(getMessage(event), "warning");
+});
+
+document.addEventListener("info", function (event) {
+    toast(getMessage(event), "info");
+});
+
+document.addEventListener("error", function (event) {
+    toast(getMessage(event), "error");
+});
--- packages/permission/vite.config.js
+++ packages/permission/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     build: {
