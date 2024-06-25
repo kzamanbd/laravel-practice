654f4f858ae5d657fe640133b25b54000d0e5f3e
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-06-25 10:18:39 +0600

    refactor: default layout

--- packages/permission/resources/views/components/lara-permission.blade.php
+++ packages/permission/resources/views/components/lara-permission.blade.php
@@ -16,6 +16,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
         <!-- Scripts -->
         {!! (new \DraftScripts\Permission\LaraPermission)->css() !!}
--- packages/permission/src/Livewire/PermissionLayout.php
+++ packages/permission/src/Livewire/PermissionLayout.php
@@ -5,7 +5,7 @@ namespace DraftScripts\Permission\Livewire;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
-#[Layout('lara-permission::components.app')]
+#[Layout('lara-permission::components.lara-permission')]
 class PermissionLayout extends Component
 {
 }
