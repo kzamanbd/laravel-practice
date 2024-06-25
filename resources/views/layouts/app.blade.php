d20cf1229a1c5bb2d9791a777574d949eddda300
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-06-25 12:49:48 +0600

    fix: asset manifest issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/permission.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
 
     <body class="font-sans antialiased">
