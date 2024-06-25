4351f2d5fe62dcbd8a8e9666a1357c168f5a3a90
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-06-25 14:42:36 +0600

    feat: added location package

---
+++ packages/bd-location/src/BdLocationServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DraftScripts\BdLocation;
+
+use Illuminate\Support\ServiceProvider;
+
+class BdLocationServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
