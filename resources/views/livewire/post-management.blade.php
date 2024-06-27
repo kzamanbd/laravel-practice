2c1b1f4070193509da0c72aea70183b53a0df98a
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-06-28 02:01:50 +0600

    Update post-management.blade.php

--- resources/views/livewire/post-management.blade.php
+++ resources/views/livewire/post-management.blade.php
@@ -10,8 +10,20 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-2">
             <div class="p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg space-y-4">
-                <textarea rows="5" id="editor" class="form-control"></textarea>
+                <textarea rows="5" id="editor" class="form-control" wire:ignore></textarea>
             </div>
         </div>
     </div>
+
+    <x-slot name="head">
+        <link href="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.snow.css" rel="stylesheet">
+    </x-slot>
+    <x-slot name="script">
+        <script src="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.js"></script>
+        <script>
+          const quill = new Quill('#editor', {
+            theme: 'snow'
+          });
+        </script>
+    </x-slot>
 </div>
