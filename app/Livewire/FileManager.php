86b842ffcfc44c4d58bba9c11f7367b68f73b61f
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-08-28 11:05:39 +0600

    feat: added file manager ui with files

--- app/Livewire/FileManager.php
+++ app/Livewire/FileManager.php
@@ -2,10 +2,48 @@
 
 namespace App\Livewire;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\File;
 use Livewire\Component;
 
 class FileManager extends Component
 {
+    // Function to recursively build the directory tree
+    function getDirectoryTree($directory)
+    {
+        $items = [];
+
+        // Get all directories in the current directory
+        foreach (File::directories($directory) as $dir) {
+            $directoryInfo = new \SplFileInfo($dir);
+            $items[] = [
+                'type' => 'directory',
+                'name' => $directoryInfo->getFilename(),
+                'path' => $directoryInfo->getPathname(),
+                'size' => $directoryInfo->getSize(),
+                'last_modified' => $directoryInfo->getMTime(),
+                'children' => $this->getDirectoryTree($dir), // Recursive call
+            ];
+        }
+
+        // Get all files in the current directory
+        foreach (File::files($directory) as $file) {
+            $items[] = [
+                'type' => 'file',
+                'name' => $file->getFilename(),
+                'path' => $file->getPathname(),
+                'size' => $file->getSize(),
+                'last_modified' => Carbon::createFromTimestamp($file->getMTime())->toDateTimeString(),
+            ];
+        }
+
+        return $items;
+    }
+    public function getFilesProperty()
+    {
+        return $this->getDirectoryTree(base_path());
+    }
+
     public function render()
     {
         return view('livewire.file-manager');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,69 +1,70 @@
-import forms from "@tailwindcss/forms";
-import colors from "tailwindcss/colors";
-import defaultTheme from "tailwindcss/defaultTheme";
+import forms from '@tailwindcss/forms';
+import colors from 'tailwindcss/colors';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const primary = colors.green;
 const colorConfig = {
     dark: {
         ...colors.slate,
-        DEFAULT: "#1F2937",
+        DEFAULT: '#1F2937'
     },
     white: {
-        DEFAULT: "#FFFFFF",
-        light: "#E0E6ED",
+        DEFAULT: '#FFFFFF',
+        light: '#E0E6ED'
     },
-    light: "#F5F8FA",
+    light: '#F5F8FA',
     primary: {
         ...primary,
-        DEFAULT: "#00A76F",
+        DEFAULT: primary[500]
     },
     info: {
         ...colors.indigo,
-        DEFAULT: colors.indigo[500],
+        DEFAULT: colors.indigo[500]
     },
     danger: {
         ...colors.rose,
-        DEFAULT: colors.rose[500],
+        DEFAULT: colors.rose[500]
     },
     success: {
         ...colors.emerald,
-        DEFAULT: colors.emerald[500],
+        DEFAULT: colors.emerald[500]
     },
     warning: {
         ...colors.amber,
-        DEFAULT: colors.amber[500],
+        DEFAULT: colors.amber[500]
     },
     secondary: {
         ...colors.gray,
-        DEFAULT: colors.gray[400],
-    },
+        DEFAULT: colors.gray[400]
+    }
 };
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.vue",
-        "./resources/js/**/*.js",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+        './resources/js/**/*.js'
     ],
 
-    darkMode: "class", // or 'media'
+    darkMode: 'class', // or 'media'
 
     theme: {
-        themeVariants: ["dark", "light"],
+        themeVariants: ['dark', 'light'],
         extend: {
             colors: colorConfig,
             maxHeight: {
-                0: "0",
-                xl: "36rem",
+                0: '0',
+                xl: '36rem'
             },
             fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            },
-        },
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans]
+            }
+        }
     },
 
-    plugins: [forms],
+    plugins: [forms]
 };
+
