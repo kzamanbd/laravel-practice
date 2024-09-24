819ad0f4f11603dd3f2414eb662e212113390e0c
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-09-24 14:08:06 +0600

    Update FileManager.php

--- packages/file-manager/src/Facades/FileManager.php
+++ packages/file-manager/src/Facades/FileManager.php
@@ -9,6 +9,21 @@ use Illuminate\Support\Facades\Facade;
  * @method static HtmlString css()
  * @method static HtmlString js()
  * @method static bool registersRoutes()
+ * @method static array scriptVariables()
+ * @method static array getDiskList()
+ * @method static bool checkDisk($name)
+ * @method static bool checkPath($disk, $path)
+ * @method static array getFileInfo(SplFileInfo $item, $pathReplace = null)
+ * @method static string formatSizeUnits($bytes)
+ * @method static array getContent($disk, $path = null)
+ * @method static array directoriesWithProperties($disk, $path = null)
+ * @method static array filesWithProperties($disk, $path = null)
+ * @method static array getDirectoriesTree($disk, $path = null)
+ * @method static array fileProperties($disk, $path = null)
+ * @method static array directoryProperties($disk, $path = null)
+ *
+ *
+ *
  * @see \DraftScripts\FileManager\FileManager
  */
 class FileManager extends Facade
