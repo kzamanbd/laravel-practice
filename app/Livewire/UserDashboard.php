0a793c5a2ae5ab1cc558365c6709ab336fe0ed52
KAMRUZZAMAN <kzamanbn@gmail.com>
2025-03-22 20:23:27 +0600

    Update dashboard.blade.php

--- app/Livewire/UserDashboard.php
+++ app/Livewire/UserDashboard.php
@@ -48,6 +48,6 @@ class UserDashboard extends Component
 
     public function render()
     {
-        return view('livewire.user-dashboard');
+        return view('dashboard');
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -14,4 +14,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
