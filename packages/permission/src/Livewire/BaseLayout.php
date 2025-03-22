8396ee3cd9a939e49bb4c88b0d52fff264b1d5df
Kamruzzaman <kzamanbn@gmail.com>
2025-03-22 19:55:35 +0600

    Remove/permision package (#6)
    * remove permission packages
    
    * Update navigation.blade.php

--- packages/permission/src/Livewire/BaseLayout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DraftScripts\Permission\Livewire;
-
-use Livewire\Attributes\Layout;
-use Livewire\Component;
-
-#[Layout('lara-permission::app')]
-class BaseLayout extends Component {}
