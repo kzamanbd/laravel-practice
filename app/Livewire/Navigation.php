4eb337e0ec0d9e92871c9f1d02e09273f4f4003c
KAMRUZZAMAN <kzamanbn@gmail.com>
2025-03-22 20:49:07 +0600

    Update Navigation.php

--- app/Livewire/Navigation.php
+++ app/Livewire/Navigation.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Actions\Logout;
 use Livewire\Component;
 
 class Navigation extends Component
 {
+    public function logout(Logout $logout): void
+    {
+        $logout();
+
+        $this->redirect('/', navigate: true);
+    }
     public function render()
     {
         return view('livewire.layout.navigation');
