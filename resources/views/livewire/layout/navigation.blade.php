0f2a058f6a5beeb0be2d5e7cc88cd842ac99d336
KAMRUZZAMAN <kzamanbn@gmail.com>
2025-03-22 19:55:08 +0600

    Update navigation.blade.php

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -28,7 +28,7 @@ new class extends Component {
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-4 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
@@ -39,7 +39,7 @@ new class extends Component {
                     </x-nav-link>
                     @endif
 
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <div class="hidden sm:flex sm:items-center">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 <button
@@ -79,7 +79,7 @@ new class extends Component {
                         </x-dropdown>
                     </div>
 
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <div class="hidden sm:flex sm:items-center">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 <button
@@ -106,39 +106,6 @@ new class extends Component {
                                 <x-dropdown-link :href="url('messaging')" target="_blank">
                                     Messaging
                                 </x-dropdown-link>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
-
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button
-                                    class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-hidden focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                    Access Control
-                                    <div class="ml-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
-
-                            <x-slot name="content">
-                                @if (Route::has('lara-permission.users'))
-                                <x-dropdown-link :href="route('lara-permission.users')">
-                                    Users List
-                                </x-dropdown-link>
-                                @endif
-
-                                @if (Route::has('lara-permission.roles'))
-                                <x-dropdown-link :href="route('lara-permission.roles')">
-                                    Roles List
-                                </x-dropdown-link>
-                                @endif
                             </x-slot>
                         </x-dropdown>
                     </div>
